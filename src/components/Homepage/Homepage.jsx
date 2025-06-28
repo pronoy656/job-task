4e185506beed086bab8625e39b66040aa800c20a
pronoy656 <pronoypaul56@gmail.com>
2025-06-28 16:29:06 +0600

    fixed color issue

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -9,23 +9,25 @@ import Link from "next/link";
 export default function Homepage() {
   return (
     <div>
-      <h1 className="bg-[#ED6923] text-white h-[50px] items-center text-center font-[400] text-[16px] font-Rancho">
+      <h1 className="bg-[#ED6923] text-white h-[50px] text-center font-[400] text-[16px] p-2.5">
         {" "}
         Free Delivery on Your First Order – Order Now!
       </h1>
+
       <div className="max-w-7xl mx-auto flex justify-between items-center mt-5">
         <img className="w-[102px] h-[56px]" src="png file 2 2.png" alt="" />
         <Link href="/profile">
           <img className="w-[65px] h-[65px]" src="Ellipse 2872.png" alt="" />
         </Link>
       </div>
-      <Banner />
+
       <div
         style={{
           background:
             "linear-gradient(to right, rgba(253, 237, 229, 0), rgba(254, 140, 79, 0.1), rgba(254, 140, 79, 0.2))",
         }}
       >
+        <Banner />
         <Category />
         <Popularfood />
         <PopularResturant />
